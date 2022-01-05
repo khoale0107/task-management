63bcf94127ce12dd49ce7459ae5b4604d5b9fb23
Khoa Le <>
2022-01-05 13:42:31 +0700

    update sql

--- web_cuoiky.sql
+++ web_cuoiky.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 01, 2022 at 08:23 AM
+-- Generation Time: Jan 05, 2022 at 07:40 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -27,7 +27,6 @@ DELIMITER $$
 --
 -- Procedures
 --
-DROP PROCEDURE IF EXISTS `bo_nhiem_truong_phong`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `bo_nhiem_truong_phong` (IN `manv` VARCHAR(20))  BEGIN
 DECLARE idphongban INT;
 
@@ -46,7 +45,6 @@ UPDATE phongban set matruongphong = manv WHERE id = idphongban;
 
 END$$
 
-DROP PROCEDURE IF EXISTS `xin_nghi_phep`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `xin_nghi_phep` (IN `manv` VARCHAR(20), IN `songay` TINYINT, IN `lydo` TEXT, IN `file` TEXT)  BEGIN
 	IF (EXISTS (SELECT * FROM nghiphep WHERE username = manv AND trangthai = 'waiting')) THEN 
 		SIGNAL sqlstate '45000' set message_text = "Không thể tạo đơn mới vì bạn có đơn khác đang chờ duyệt.";
@@ -67,7 +65,6 @@ DELIMITER ;
 -- Table structure for table `account`
 --
 
-DROP TABLE IF EXISTS `account`;
 CREATE TABLE `account` (
   `username` varchar(20) NOT NULL,
   `password` varchar(100) NOT NULL,
@@ -82,17 +79,17 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`username`, `password`, `hoten`, `chucvu`, `maphongban`, `avatar`) VALUES
-('51900751', '$2y$10$AAFWHNLuXPsVAQ.vY65az./FUOxy2cd61Bixve2KgIq7quKemmflG', 'Khoa Võ', 'Trưởng phòng', 1, 'fc5c699a27f5cc4e48d496e5c351549c_avatar.png'),
-('51900752', '$2y$10$tTInCeI7DbOMZYtVNmewOeT2QmjLc5vPQRmjDYMoCR2JzyEU9t38q', 'Khoa Trần', 'Nhân viên', 1, '0fa1ebd1ec241c8a6db5b532d0c1607e_avatar.png'),
-('51900753', '$2y$10$YOH5ADKXpFGfjS1zw/eaceRADktqisJchuL.gmDufOSMVSF8KAA1C', 'Khoa Lê ', 'Nhân viên', 1, '00661c5fd8977f6191332e42f9d604d7_avatar.png'),
-('51900754', '$2y$10$0it7sS7dXJoM3Q5vFdE7qe2ei3etOfKyyIE4UsSAgIERfC1XZloE6', 'Khoa Phan', 'Nhân viên', 1, 'e608a2f52da846f00ad2e0a5200b7cb5_avatar.png'),
-('51900755', '$2y$10$qyMNLJUMIJDGZGqPV4DXvOMr8NOIhDX/0qigZS7OlJfYFb971RBy6', 'Khoa Phạm', 'Nhân viên', 1, 'default_avatar.png'),
-('51900756', '$2y$10$A/GCNsHCEHm1R4ejTja26OvBf.gbo6AQkKLSW25F8weAbKpYBlJWq', 'Khoa Nguyễn', 'Nhân viên', 2, '04aa6d8cd248daeb3f356079a9f4bda1_avatar.png'),
-('51900757', '$2y$10$dfAU2aK7LBLSoBd3DHilceDMpSzneGaQn/jiDMYic6pI8/pmJoQuK', 'Hiếu Phan', 'Trưởng phòng', 2, 'default_avatar.png'),
-('51900758', '$2y$10$vLmqoGo8u/6IdUqVSY8pZ.D6x8CIGLUvo0Tm46sVeVBD1k7FB036q', 'Hiếu Huỳnh', 'Nhân viên', 2, 'default_avatar.png'),
+('51900751', '$2y$10$AAFWHNLuXPsVAQ.vY65az./FUOxy2cd61Bixve2KgIq7quKemmflG', 'Khoa  Võ ', 'Trưởng phòng', 1, '177ee0eed34afd29b850dc57759fc32e_avatar.png'),
+('51900752', '$2y$10$tEc8qiS38K/9XMhsiAU.RuJHDPB.tiVPUBsZG8rIJc.vaxfDIpLEe', 'Khoa Trần', 'Nhân viên', 1, 'f433a4924ff2681ff5fce424a537ef81_avatar.png'),
+('51900753', '$2y$10$YOH5ADKXpFGfjS1zw/eaceRADktqisJchuL.gmDufOSMVSF8KAA1C', 'Khoa Lê ', 'Nhân viên', 1, '8047b1a0637f6696f2e0b3a305fa3889_avatar.png'),
+('51900754', '$2y$10$0it7sS7dXJoM3Q5vFdE7qe2ei3etOfKyyIE4UsSAgIERfC1XZloE6', 'Khoa Phan', 'Nhân viên', 1, '5a6001401eec210fe055abad5d307521_avatar.png'),
+('51900755', '$2y$10$qyMNLJUMIJDGZGqPV4DXvOMr8NOIhDX/0qigZS7OlJfYFb971RBy6', 'Khoa Phạm', 'Nhân viên', 1, 'e34dc03dca60bda11cf62bc74d6069a5_avatar.png'),
+('51900756', '$2y$10$A/GCNsHCEHm1R4ejTja26OvBf.gbo6AQkKLSW25F8weAbKpYBlJWq', 'Khoa Nguyễn', 'Nhân viên', 2, '861c71f99761063252bbdfa253d204c9_avatar.png'),
+('51900757', '$2y$10$dfAU2aK7LBLSoBd3DHilceDMpSzneGaQn/jiDMYic6pI8/pmJoQuK', 'Hiếu Phan', 'Trưởng phòng', 2, '86dcb6b309d84d4ae7f1896d69b26add_avatar.png'),
+('51900758', '$2y$10$vLmqoGo8u/6IdUqVSY8pZ.D6x8CIGLUvo0Tm46sVeVBD1k7FB036q', 'Hiếu Huỳnh', 'Nhân viên', 2, '1c04397ae951d23a423b0dda4de88b4d_avatar.png'),
 ('51900759', '$2y$10$0FVDA.e6NYSo4xzmC0iUgOrkw7SEx.h5UrJP56b9yZyy4KS4TQqBi', 'Hiếu Nguyễn ', 'Nhân viên', 1, 'default_avatar.png'),
 ('51900760', '$2y$10$sWEifhnHWEI8RFvObsDIdeCpHC1NAYRxhFqlmqSmdwd0VYkYiMej.', 'Hiếu Trần', 'Nhân viên', 2, 'default_avatar.png'),
-('51900761', '$2y$10$vQk.HLe0rIZ9Pgmv4U2DPOMC3OKzj8Z8GlpAzxl0G63ODsTPaHxcu', 'Hiếu Phạm', 'Nhân viên', 1, '09a7349202ed2f5c90660f61289fe729_avatar.png'),
+('51900761', '$2y$10$vQk.HLe0rIZ9Pgmv4U2DPOMC3OKzj8Z8GlpAzxl0G63ODsTPaHxcu', 'Hiếu Phạm', 'Nhân viên', 1, 'default_avatar.png'),
 ('51900762', '$2y$10$bUpfKgFR0oTYV9X8iZJQA.cMcsJrrJfnu/HWqcpdwzs/4pag2CZk6', 'Hiếu Lê', 'Nhân viên', 1, 'default_avatar.png'),
 ('51900763', '$2y$10$xIaAz2UYolwNTexZZjehZeTJBpx1Mu16mH5nOjXxqAvbiyqesXkVS', 'Hiếu Võ', 'Nhân viên', 1, 'default_avatar.png'),
 ('51900764', '$2y$10$/O4htEVlwTBKwIW4MDHqh.mdtJ5NNDWLhovUowAGUwdlUqk235psO', 'Hiếu Vương', 'Nhân viên', 3, 'default_avatar.png'),
@@ -110,7 +107,6 @@ INSERT INTO `account` (`username`, `password`, `hoten`, `chucvu`, `maphongban`, 
 -- Table structure for table `nghiphep`
 --
 
-DROP TABLE IF EXISTS `nghiphep`;
 CREATE TABLE `nghiphep` (
   `ID` int(11) NOT NULL,
   `username` varchar(20) NOT NULL,
@@ -135,15 +131,16 @@ INSERT INTO `nghiphep` (`ID`, `username`, `songay`, `lydo`, `file`, `ngaylap`, `
 (30, '51900754', 1, 'lý do 1', 'keel_gl.dll', '2021-12-29 12:46:00', 'approved'),
 (31, '51900754', 1, 'lý do 2', '', '2021-12-30 23:11:00', 'approved'),
 (32, '51900754', 1, 'lý do 1', '', '2021-12-30 20:51:00', 'refused'),
-(33, '51900760', 2, 'abcxy', '123.txt', '2021-12-30 21:22:00', 'waiting'),
+(33, '51900760', 2, 'abcxy', '123.txt', '2021-12-30 21:22:00', 'refused'),
 (34, '51900761', 0, '', '', '2021-12-30 13:22:00', 'refused'),
 (35, '51900755', 4, '123123', 'jcpicker.ini', '2021-12-30 16:05:00', 'approved'),
-(36, '51900751', 1, 'asd', '', '2021-12-30 21:33:00', 'approved'),
 (37, '51900765', 2, 'toi la Hiếu Vũ', '', '2021-12-31 20:01:08', 'approved'),
 (38, '51900766', 2, 'tôi là Hiếu Trương', '', '2021-12-31 20:01:46', 'approved'),
-(39, '51900756', 1, 'tôi là 51900756 Khoa Nguyễn', '', '2021-12-31 20:32:25', 'waiting'),
+(39, '51900756', 1, 'tôi là 51900756 Khoa Nguyễn', '', '2021-12-31 20:32:25', 'approved'),
 (45, '51900757', 14, 'trưởng phòng Hiếu phan xin nghỉ', '', '2021-12-31 21:14:26', 'refused'),
-(48, '51900759', 3, 'Hiếu Nguyễn yêu cầu nghỉ 3 ngày', 'LICENSE.txt', '2022-01-01 12:30:39', 'refused');
+(48, '51900759', 3, 'Hiếu Nguyễn yêu cầu nghỉ 3 ngày', 'LICENSE.txt', '2022-01-01 12:30:39', 'refused'),
+(49, '51900751', 5, 'trưởng phòng Khoa Võ xin nghỉ 5 ngày', '1.png', '2022-01-01 14:32:58', 'approved'),
+(51, '51900758', 5, 'Hiếu Huỳnh -51900758 nghỉ 5 ngày', '1 (9).txt', '2022-01-05 13:32:40', 'waiting');
 
 -- --------------------------------------------------------
 
@@ -151,7 +148,6 @@ INSERT INTO `nghiphep` (`ID`, `username`, `songay`, `lydo`, `file`, `ngaylap`, `
 -- Table structure for table `phongban`
 --
 
-DROP TABLE IF EXISTS `phongban`;
 CREATE TABLE `phongban` (
   `ID` smallint(6) NOT NULL,
   `tenphongban` varchar(20) NOT NULL,
@@ -173,7 +169,6 @@ INSERT INTO `phongban` (`ID`, `tenphongban`, `matruongphong`, `mota`, `sophong`)
 --
 -- Triggers `phongban`
 --
-DROP TRIGGER IF EXISTS `bo_nhiem_truong_phong`;
 DELIMITER $$
 CREATE TRIGGER `bo_nhiem_truong_phong` AFTER UPDATE ON `phongban` FOR EACH ROW BEGIN
 	IF (NEW.matruongphong <=> NULL OR NEW.matruongphong <=> '') THEN 
@@ -187,6 +182,23 @@ CREATE TRIGGER `bo_nhiem_truong_phong` AFTER UPDATE ON `phongban` FOR EACH ROW B
 END
 $$
 DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE `task` (
+  `ID` int(11) NOT NULL,
+  `taskname` text NOT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `trangthai` text NOT NULL DEFAULT 'New',
+  `updatetime` datetime NOT NULL DEFAULT current_timestamp(),
+  `duedate` datetime NOT NULL,
+  `mota` text NOT NULL,
+  `file` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -214,6 +226,13 @@ ALTER TABLE `phongban`
   ADD KEY `fk-truongphong` (`matruongphong`);
 
 --
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `fk-task-account` (`username`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -221,13 +240,19 @@ ALTER TABLE `phongban`
 -- AUTO_INCREMENT for table `nghiphep`
 --
 ALTER TABLE `nghiphep`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT for table `phongban`
 --
 ALTER TABLE `phongban`
   MODIFY `ID` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `task`
+--
+ALTER TABLE `task`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -250,6 +275,12 @@ ALTER TABLE `nghiphep`
 --
 ALTER TABLE `phongban`
   ADD CONSTRAINT `fk-truongphong` FOREIGN KEY (`matruongphong`) REFERENCES `account` (`username`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `task`
+--
+ALTER TABLE `task`
+  ADD CONSTRAINT `fk-task-account` FOREIGN KEY (`username`) REFERENCES `account` (`username`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
